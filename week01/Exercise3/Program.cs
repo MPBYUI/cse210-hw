198f29d75ef7d8c15fd19832f683c910f9a41593
MPBYUI <mpeel@byupathway.edu>
2025-07-04 16:12:58 -0600

    exercise 3 w1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // string response;
+        // do
+        // {
+        //     Console.WriteLine("What is the magic number");
+        //     string magic = Console.ReadLine();
+        //     int number = int.Parse(magic);
+        //     Console.WriteLine($"Debug: The number is {number}");
+        //     Console.Write("Do you want to continue? ");
+        //     response = Console.ReadLine();
+        // } while (response == "yes");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int response = -1;
+
+        while (response != number)
+        {
+            Console.WriteLine($"The rng is {number}");
+            Console.WriteLine("What is your guess? ");
+            response = int.Parse(Console.ReadLine());
+
+            if (number > response)
+            {
+                Console.WriteLine("Guess Higher");
+            }
+
+            else if (number < response)
+            {
+                Console.WriteLine("Guess Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it");
+            }
+        }
+
     }
 }
