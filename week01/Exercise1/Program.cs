7e84c896e5f5a23730d6d33454b3ca807ca27857
MPBYUI <mpeel@byupathway.edu>
2025-07-03 19:21:13 -0600

    Exercise 2 w1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -13,3 +13,4 @@ class Program
         Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
+
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.WriteLine("What is your grade number? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+        Console.WriteLine(number);
+
+        if (number >= 90)
+        {
+            Console.WriteLine("Your Grade Is A");
+            Console.WriteLine("You passed the class.");
+        }
+        else if (number >= 80)
+        {
+            Console.WriteLine("Your Grade Is B");
+            Console.WriteLine("You passed the class.");
+        }
+        else if (number >= 70)
+        {
+            Console.WriteLine("Your Grade Is C");
+            Console.WriteLine("You passed the class.");
+        }
+        else if (number >= 60)
+        {
+            Console.WriteLine("Your grade is D");
+            Console.WriteLine("You failed the class.");
+        }
+        else if (number < 60)
+        {
+            Console.WriteLine("Your Grade Is F");
+            Console.WriteLine("YOU LOSE");
+        }
     }
 }
+
+// 
+// A >= 90
+// B >= 80
+// C >= 70
+// D >= 60
+// F < 60
+// 
