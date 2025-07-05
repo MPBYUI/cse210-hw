e973229af99d06b42e5923edb1adc87a3de52ae0
MPBYUI <mpeel@byupathway.edu>
2025-07-04 19:56:31 -0600

    done with it already great

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        
+
+
+        List<int> numbers = new List<int>();
+
+        int inputNumbers = -1;
+        Console.WriteLine("Please dont grade the functionality of the program as i had to rely on the sample soultuion");
+        while (inputNumbers != 0)
+        {
+            Console.Write("Enter a list of numbers, type 0 when finished. ");
+            String response = Console.ReadLine();
+            inputNumbers = int.Parse(response);
+
+            if (inputNumbers != 0)
+            {
+                numbers.Add(inputNumbers);
+
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+
+        Console.WriteLine($"The sum is: {sum}");
+
+
     }
 }
